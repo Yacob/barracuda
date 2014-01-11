1f5b9497b73490948fc538e578b6cf7c546ed38b
yacob <nestor.jac@gmail.com>
2014-01-10 21:41:48 -0500

    Refactor code
    Refactor furthest current code
    Add functions ...

--- player.py
+++ player.py
@@ -19,71 +19,10 @@ def sample_bot(host, port):
 			sys.exit(1)
 
 		elif msg["type"] == "request":
-			if msg["state"]["game_id"] != gameId:
-				gameId = msg["state"]["game_id"]
-				print("New game started: " + str(gameId))
-			if msg["can_challenge"] and msg["state"]["your_tricks"] >= 3:
-				s.send({
-					"type": "offer_challenge"
-				})
-			if msg["request"] == "request_card":
-				card_to_play = -1
-
-				# sort hand
-				hand = msg["state"]["hand"]
-				hand.sort()
-
-				# responding to played card
-				if "card" in msg["state"]:
-					value = msg["state"]["card"]
-					for card in hand:
-						if card > value:
-							card_to_play = card;
-							break;
-
-					if card_to_play == -1:
-						card_to_play = hand[0]
-
-
-				# leading with a card
-			else:
-				card_to_play = hand[0]
-
-				s.send({
-					"type": "move",
-					"request_id": msg["request_id"],
-					"response": {
-						"type": "play_card",
-						"card": card_to_play
-						}
-					})
-
-
-		elif msg["request"] == "challenge_offered":
-			if msg["state"]["your_tricks"] >= msg["state"]["their_tricks"]:
-				hand_value = 0
-				for card in msg["state"]["hand"]:
-					hand_value += card
-					if (hand_value / (5 - msg["state"]["total_tricks"])) >= 11:
-						s.send({
-							"type": "move",
-							"request_id": msg["request_id"],
-							"response": {
-								"type": "accept_challenge"
-							}
-						})
-			else:
-				s.send({
-					"type": "move",
-					"request_id": msg["request_id"],
-					"response": {
-						"type": "reject_challenge"
-					}
-				})
-
+			respond_to_request(msg)
 		elif msg["type"] == "greetings_program":
-				print("Connected to the server.")
-
+			print("connected to the server.")
+			
 def loop(player, *args):
 	while True:
 		try:
@@ -93,6 +32,86 @@ def loop(player, *args):
 		except Exception as e:
 			print(repr(e))
 		time.sleep(10)
+
+
+def respond_to_request(msg):
+	# message type is request
+	if msg["state"]["game_id"] != gameId:
+		gameId = msg["state"]["game_id"]
+		print("New game started: " + str(gameId))
+	if msg["can_challenge"] and msg["state"]["your_tricks"] >= 3:
+		s.send({
+			"type": "offer_challenge"
+		})
+	elif msg["request"] == "request_card":
+		play_card(msg)
+
+	elif msg["request"] == "challenge_offered":
+		respond_to_challenge(msg)
+
+
+def play_card(msg):
+	card_to_play = -1
+	# sort hand
+	hand = msg["state"]["hand"]
+	hand.sort()
+	# responding to played card
+	if "card" in msg["state"]:
+		value = msg["state"]["card"]
+		for card in hand:
+			if card > value:
+				card_to_play = card;
+				break;
+		if card_to_play == -1:
+			card_to_play = hand[0]
+
+
+		# leading with a card
+	else:
+		card_to_play = hand[0]
+		s.send({
+			"type": "move",
+			"request_id": msg["request_id"],
+			"response": {
+				"type": "play_card",
+				"card": card_to_play
+				}
+			})
+
+
+
+
+def respond_to_challenge(msg):
+	if msg["state"]["your_tricks"] >= msg["state"]["their_tricks"]:
+		hand_value = 0
+		for card in msg["state"]["hand"]:
+			hand_value += card
+		if (hand_value / (5 - msg["state"]["total_tricks"])) >= 11:
+			s.send({
+				"type": "move",
+				"request_id": msg["request_id"],
+				"response": {
+					"type": "accept_challenge"
+					}
+				})
+		else:
+			s.send({
+				"type": "move",
+				"request_id": msg["request_id"],
+				"response": {
+					"type": "reject_challenge"
+				}
+			})
+	else:
+		s.send({
+			"type": "move",
+			"request_id": msg["request_id"],
+			"response": {
+				"type": "reject_challenge"
+			}
+		})
+
+
 
 class SocketLayer:
 	def __init__(self, host, port):
